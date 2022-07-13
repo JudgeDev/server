b30bd730c5d54fc12183e0d3ae0b066905d3d2ca
jd <judge.py@gmail.com>
2022-07-13 10:34:21 +0100

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,8 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from django.urls import resolve
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self) -> None:
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self) -> None:
-        request = HttpRequest()  # what django sees when asked for a page
-        response = home_page(request)  # response from a view
-        html = response.content.decode("utf8")  # decode raw bites
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.endswith("</html>"))
+    def test_uses_home_template(self) -> None:
+        response = self.client.get("/")  # call view directly
+        # check correct template was used
+        self.assertTemplateUsed(response, "home.html")
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
-# from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 
 # Views for the lists app
 def home_page(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<html><title>To-Do lists</title></html>")
+    return render(request, "home.html")
