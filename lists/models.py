3109ab9c78a6e3601c8c9a273e74c6029a4adeae
jd <judge.py@gmail.com>
2022-07-13 14:55:44 +0100

    Model for list Items and associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,16 @@
-# from django.db import models
+""" List app models
 
-# Create your models here.
+The ORM models the db.
+Classes that inherit from models.Model map to tables in the db.
+Build the database with migrations
+- it modifies the db based on these files.
+Classes get an auto-generated id attribute, which will be a primary key
+column in the database.
+Need to define any other columns explicitly
+"""
+from django.db import models
+
+
+# List app models
+class Item(models.Model):
+    text = models.TextField(default="")  # define text field column
