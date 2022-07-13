a89b0ee15f43d7aabac58beabfc45d7466c46c87
jd <judge.py@gmail.com>
2022-07-13 16:14:16 +0100

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,8 @@
+""" Unit tests for the lists app
+
+Each test should test one thing
+Divide test into: Setup, Exercise, Assert sections
+"""
 from django.test import TestCase
 
 from lists.models import Item
@@ -10,9 +15,28 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
     def test_can_save_a_POST_request(self) -> None:
+        self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self) -> None:
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertIn("A new list item", response.content.decode())
-        self.assertTemplateUsed(response, "home.html")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
+
+    def test_only_saves_items_when_necessary(self) -> None:
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self) -> None:
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertIn("itemey 1", response.content.decode())
+        self.assertIn("itemey 2", response.content.decode())
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,20 @@
+""" Views for lists app
+
+Views should process user input and return appropriate response.
+"""
+
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from lists.models import Item
 
 
 # Views for the lists app
 def home_page(request: HttpRequest) -> HttpResponse:
-    # render request with given template and template variable dict
-    return render(
-        request,
-        "home.html",
-        {
-            "new_item_text": request.POST.get("item_text", ""),
-        },
-    )
+    if request.method == "POST":
+        # create and store new item with POST contents
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+    items = Item.objects.all()  # get all table items
+    # render request with home template and table items
+    return render(request, "home.html", {"items": items})
