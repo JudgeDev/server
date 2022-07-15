685a464a4c95e89710a890c10e86ba6612d0b876
jd <judge.py@gmail.com>
2022-07-15 11:10:09 +0100

    New url, view and tamplate to display lists

--- lists/views.py
+++ lists/views.py
@@ -11,10 +11,17 @@ from lists.models import Item
 
 # Views for the lists app
 def home_page(request: HttpRequest) -> HttpResponse:
+    """Home page view"""
     if request.method == "POST":
         # create and store new item with POST contents
         Item.objects.create(text=request.POST["item_text"])
-        return redirect("/")
+        return redirect("/lists/the-only-list-in-the-world/")
+    # render request with home template
+    return render(request, "home.html")
+
+
+def view_list(request: HttpRequest) -> HttpResponse:
+    """View an existing todo list"""
     items = Item.objects.all()  # get all table items
     # render request with home template and table items
-    return render(request, "home.html", {"items": items})
+    return render(request, "list.html", {"items": items})
