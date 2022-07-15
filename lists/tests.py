951d0583314ab0d5902f7c03c63bde70d6aab80e
jd <judge.py@gmail.com>
2022-07-15 14:27:30 +0100

    new URL, view and template for new lists

--- lists/tests.py
+++ lists/tests.py
@@ -16,19 +16,6 @@ class HomePageTest(TestCase):
         # check correct template was used
         self.assertTemplateUsed(response, "home.html")
 
-    def test_can_save_a_POST_request(self) -> None:
-        self.client.post("/", data={"item_text": "A new list item"})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
-
-    def test_redirects_after_POST(self) -> None:
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(
-            response["location"], "/lists/the-only-list-in-the-world/"
-        )
-
     def test_only_saves_items_when_necessary(self) -> None:
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
@@ -68,3 +55,18 @@ class ListViewTest(TestCase):
         # assertIn ("itemey 1", response.content.decode())
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self) -> None:
+        # urls without trailing slash -> modify the database
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self) -> None:
+        response = self.client.post(
+            "/lists/new", data={"item_text": "A new list item"}
+        )
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
--- lists/views.py
+++ lists/views.py
@@ -12,11 +12,6 @@ from lists.models import Item
 # Views for the lists app
 def home_page(request: HttpRequest) -> HttpResponse:
     """Home page view"""
-    if request.method == "POST":
-        # create and store new item with POST contents
-        Item.objects.create(text=request.POST["item_text"])
-        return redirect("/lists/the-only-list-in-the-world/")
-    # render request with home template
     return render(request, "home.html")
 
 
@@ -25,3 +20,8 @@ def view_list(request: HttpRequest) -> HttpResponse:
     items = Item.objects.all()  # get all table items
     # render request with home template and table items
     return render(request, "list.html", {"items": items})
+
+
+def new_list(request: HttpRequest) -> HttpResponse:
+    Item.objects.create(text=request.POST["item_text"])
+    return redirect("/lists/the-only-list-in-the-world/")
