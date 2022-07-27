925efa6d0806a956044a5fa6654bcb68ec088549
jd <judge.py@gmail.com>
2022-07-27 14:12:13 +0100

    Add onload boilerplate and namespace for list.js functions

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,8 +1,7 @@
-var initialize = function () {
-    console.log('initialize called');
+window.Server = {};  // namespace as property of window global
+window.Server.initialize = function () {  // function is attribute of Server namespace
     // find input elements called text, on keypress, hide all elements with class .has_error
     $('input[name="text"]').on('keypress', function () {
         $('.has-error').hide();
     });
 };
-console.log('list.js loaded');
