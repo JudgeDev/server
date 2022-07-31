ddb4e7cd79da0dd2760f24113dc579635fa6f43d
jd <judge.py@gmail.com>
2022-07-31 09:05:33 +0100

    Add __str__ functions to blog models

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,10 @@ class Category(models.Model):
     # single char field with name of category
     name = models.CharField(max_length=20)
 
+    def __str__(self) -> str:
+        # display name of instance, eg in admin
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -15,6 +19,10 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     # allow access to relationship from category object
     categories = models.ManyToManyField("Category", related_name="posts")
+
+    def __str__(self) -> str:
+        # display name of instance, eg in admin
+        return self.title
 
 
 class Comment(models.Model):
